7c8784b9e43c524b9be7789a9f758531d8315b7f
yorman <yorman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 04:50:08 +0000

    Added. Avoid directory listing
    git-svn-id: https://plugins.svn.wordpress.org/sucuri-scanner/trunk@905279 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/images/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Avoid directory listing.
+ *
+ * @package   Sucuri Plugin - SiteCheck Malware Scanner
+ * @author    Yorman Arias <yorman.arias@sucuri.net>
+ * @author    Daniel Cid   <dcid@sucuri.net>
+ * @copyright Since 2010 Sucuri Inc.
+ * @license   Released under the GPL - see LICENSE file for details.
+ * @link      https://wordpress.sucuri.net/
+ * @since     File available since Release 0.1
+ */
+
+if( !defined('SUCURISCAN') ){ exit(0); }
