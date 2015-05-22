16a6a267628257fa02942635c2d6aac989b4b846
yorman <yorman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 13:08:51 +0000

    Fixed. Support reverse proxy only when user decides
    git-svn-id: https://plugins.svn.wordpress.org/sucuri-scanner/trunk@1165714 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sucuri.php
+++ sucuri.php
@@ -6298,7 +6298,7 @@ class SucuriScanInterface {
      * @return void
      */
     public static function initialize(){
-        if ( SucuriScan::is_behind_cloudproxy() ) {
+        if ( SucuriScan::support_reverse_proxy() ) {
             $_SERVER['SUCURIREAL_REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
             $_SERVER['REMOTE_ADDR'] = SucuriScan::get_remote_addr();
         }
