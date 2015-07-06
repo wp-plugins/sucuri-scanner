6888ecf8952ef77ebba9848bc2e3e5e60fe67722
yorman <yorman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-06 21:47:02 +0000

    Added. Self-contained library to handle the hardening process
    git-svn-id: https://plugins.svn.wordpress.org/sucuri-scanner/trunk@1193688 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sucuri.php
+++ sucuri.php
@@ -7822,6 +7822,146 @@ function sucuriscan_monitoring_dates( $type = '', $date = '', $in_html = true ){
 }
 
 /**
+ * Project hardening library.
+ *
+ * In computing, hardening is usually the process of securing a system by
+ * reducing its surface of vulnerability. A system has a larger vulnerability
+ * surface the more functions it fulfills; in principle a single-function system
+ * is more secure than a multipurpose one. Reducing available vectors of attack
+ * typically includes the removal of unnecessary software, unnecessary usernames
+ * or logins and the disabling or removal of unnecessary services.
+ *
+ * There are various methods of hardening Unix and Linux systems. This may
+ * involve, among other measures, applying a patch to the kernel such as Exec
+ * Shield or PaX; closing open network ports; and setting up intrusion-detection
+ * systems, firewalls and intrusion-prevention systems. There are also hardening
+ * scripts and tools like Bastille Linux, JASS for Solaris systems and
+ * Apache/PHP Hardener that can, for example, deactivate unneeded features in
+ * configuration files or perform various other protective measures.
+ */
+class SucuriScanHardening extends SucuriScan {
+
+    /**
+     * Returns a list of access control rules for the Apache web server that can be
+     * used to deny and allow certain files to be accessed by certain network nodes.
+     * Currently supports Apache 2.2 and 2.4 and denies access to all PHP files with
+     * any mixed extension case.
+     *
+     * @return array List of access control rules.
+     */
+    private static function get_rules(){
+        return array(
+            '<FilesMatch "\.(?i:php)$">',
+            '  <IfModule !mod_authz_core.c>',
+            '    Deny from all',
+            '  </IfModule>',
+            '  <IfModule mod_authz_core.c>',
+            '    Require all denied',
+            '  </IfModule>',
+            '</FilesMatch>',
+        );
+    }
+
+    /**
+     * Adds some rules to an existing access control file (or creates it if does not
+     * exists) to deny access to all files with certain extension in any mixed case.
+     * The permissions to modify the file are checked before anything else, this
+     * function is self-contained.
+     *
+     * @param  string  $directory Valid directory path where to place the access rules.
+     * @return boolean            True if the rules are successfully added, false otherwise.
+     */
+    public static function harden_directory( $directory = '' ){
+        if (
+            file_exists( $directory)
+            && is_writable( $directory )
+            && is_dir( $directory )
+        ) {
+            $fhandle = false;
+            $target = $directory . '/.htaccess';
+            $deny_rules = self::get_rules();
+
+            if ( file_exists( $target ) ) {
+                $fhandle = @fopen( $target, 'a' );
+            } else {
+                $fhandle = @fopen( $target, 'w' );
+            }
+
+            if ( $fhandle ) {
+                $rules_str = implode( "\n", $deny_rules );
+                $written = fwrite( $fhandle, $rules_str );
+                fclose( $fhandle );
+
+                return (bool) ( $written !== false );
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Deletes some rules from an existing access control file to allow access to
+     * all files with certain extension in any mixed case. The file is truncated if
+     * after the operation its size is equals to zero.
+     *
+     * @param  string  $directory Valid directory path where to access rules are.
+     * @return boolean            True if the rules are successfully deleted, false otherwise.
+     */
+    public static function unharden_directory( $directory = '' ){
+        if ( self::is_hardened( $directory ) ) {
+            $deny_rules = self::get_rules();
+            $fpath = $directory . '/.htaccess';
+            $content = @file_get_contents( $fpath );
+
+            if ( $content ) {
+                $rules_str = implode( "\n", $deny_rules );
+                $content = str_replace( $rules_str, '', $content );
+                $written = @file_put_contents( $fpath, $content );
+
+                if ( filesize( $fpath ) === 0 ) {
+                    @unlink( $fpath );
+                }
+
+                return (bool) ( $written !== false );
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check whether a directory is hardened or not.
+     *
+     * @param  string  $directory Valid directory path.
+     * @return boolean            True if the directory is hardened, false otherwise.
+     */
+    public static function is_hardened( $directory = '' ){
+        if (
+            file_exists( $directory )
+            && is_dir( $directory )
+        ) {
+            $fpath = $directory . '/.htaccess';
+
+            if (
+                file_exists( $fpath )
+                && is_readable( $fpath )
+            ) {
+                $rules = self::get_rules();
+                $rules_str = implode( "\n", $rules );
+                $content = @file_get_contents( $fpath );
+
+                if ( strpos( $content, $rules_str ) !== false ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
+
+/**
  * Sucuri one-click hardening page.
  *
  * It loads all the functions defined in /lib/hardening.php and shows the forms
